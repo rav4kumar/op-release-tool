ebbc118b1e1cb6fa129db96985450dd95691e10f
Shane Smiskol <shane@smiskol.com>
2020-03-10 01:29:52 -0500

    Initial commit

---
+++ op_releaser.py
@@ -0,0 +1,82 @@
+import datetime
+import subprocess
+from utils.eta_tool import ETATool
+
+
+up_speed = 1.2433  # in megabits, can replace with your github upload speed to get more accurate eta results
+
+
+class opReleaser:
+  def __init__(self):
+    """
+      self.op_base_dir: Replace with the path leading to your local openpilot repository
+      self.release_branch: Replace with the name of your most stable branch
+      self.target_branch: Replace with the name of the target branch you want a squashed version of your release branch on
+      self.commit_message: The commit message to be pushed to your target_branch. You can either use the included date function or remove it
+    """
+
+    self.op_base_dir = 'C:/Git/op-smiskol/openpilot'
+    self.release_branch = 'stock_additions'
+    self.target_branch = 'stock_additions-release'
+    self.commit_message = 'Stock Additions 0.2 (0.7.1) {} Release'.format(self.get_cur_date())
+
+    self.eta_tool = ETATool(self.op_base_dir, up_speed)
+
+    self.msg_count = 0
+    self.total_steps = 3
+
+    self.create_release()
+
+  def create_release(self):
+    # Checkout release branch
+    self.run('git checkout -f {}'.format(self.release_branch))
+
+    # Delete old target branch if it exists
+    self.attempt_delete()
+
+    # Checkout a new orphan branch
+    self.run('git checkout --orphan {}'.format(self.target_branch))
+
+    # Add all files in release branch to new target branch
+    self.message('Adding and committing all files to {} branch...'.format(self.target_branch))
+    self.run('git add --all')
+
+    # Commit all files
+    r = self.run(['git', 'commit', '-am', self.commit_message], no_convert=True)
+
+    if 'create mode' not in r:
+      raise Exception('Error adding files to current branch!')
+
+    self.eta_tool.start_eta()
+
+    # Replace current release on remote
+    self.message('Now force pushing to remote (origin/{})...'.format(self.target_branch))
+    self.run('git push -f --set-upstream origin {}'.format(self.target_branch))
+    self.eta_tool.stop()
+
+    # Finished
+    print('\nFinished! Squashed {} branch to 1 commit and force pushed to {} branch!'.format(self.release_branch, self.target_branch))
+    print('Commit message: {}'.format(self.commit_message))
+
+  def run(self, cmd, no_convert=False):
+    if not no_convert:
+      cmd = cmd.split(' ')
+    return subprocess.check_output(cmd, cwd=self.op_base_dir, stderr=subprocess.STDOUT, encoding='utf8')
+
+  def attempt_delete(self):
+    try:
+      r = self.run('git branch -D {}'.format(self.target_branch))
+      self.message(r.replace('\n', ''))
+    except subprocess.CalledProcessError:
+      self.message('{} branch already deleted.'.format(self.target_branch))
+
+  def get_cur_date(self):
+    today = datetime.datetime.today()
+    return today.strftime('%h %d, %Y').replace(' 0', ' ')
+
+  def message(self, msg):
+    print('[{}/{}]: {}'.format(self.msg_count + 1, self.total_steps, msg), flush=True)
+    self.msg_count += 1
+
+
+opReleaser()
---
+++ utils/eta_tool.py
@@ -0,0 +1,78 @@
+import datetime
+import threading
+import os
+import time
+
+
+BYTE_TO_MB = 1048576
+push_metrics = {'size': 147.68270778656006, 'time': 62}  # used to calculate push eta
+up_speed_constant = 6.436  # used in up speed consideration
+
+
+class ETATool:
+  def __init__(self, op_base_dir, up_speed):  # only supports up to minutes
+    self.op_base_dir = op_base_dir
+    self.up_speed = up_speed
+
+    self.print_freq = 15  # seconds
+
+    self.last_print_time = time.time()
+    self.to_run = True
+
+  def start_eta(self):
+    target_size = self.calculate_target_size()
+    estimated_time = (target_size * push_metrics['time']) / push_metrics['size']
+    estimated_time /= (self.up_speed / 8) * up_speed_constant  # speed consideration
+    threading.Thread(target=self.eta_thread, args=(estimated_time,)).start()
+
+  def eta_thread(self, estimated_time):
+    start_time = time.time()
+    while self.to_run:
+      if time.time() - self.last_print_time > self.print_freq:
+        print('Pushing... ETA: {}'.format(self.get_eta(estimated_time - (time.time() - start_time))))
+        self.last_print_time = time.time()
+        time.sleep(self.print_freq / 2.0)
+      if time.time() - start_time > estimated_time:
+        print('Any second now...')
+        return
+
+  def calculate_target_size(self):
+    repo_size = self.folder_size(self.op_base_dir)
+    _git_size = self.folder_size('{}/.git'.format(self.op_base_dir))
+    return (repo_size - _git_size) / BYTE_TO_MB
+
+  def folder_size(self, path):
+    total = 0
+    for entry in os.scandir(path):
+      if entry.is_file():
+        total += entry.stat().st_size
+      elif entry.is_dir():
+        total += self.folder_size(entry.path)
+    return total
+
+  def stop(self):
+    self.to_run = False
+
+  def get_eta(self, etr):
+    sec_string = ''
+    min_string = ''
+    etr = datetime.timedelta(seconds=round(etr))
+    etr = str(etr).split(':')[-2:]
+    minutes = int(etr[0])
+    seconds = int(etr[1])
+
+    sec_string += '{} second'.format(seconds)
+    min_string += '{} minute'.format(minutes)
+
+    if seconds != 1:
+      sec_string += 's'
+    if minutes != 1:
+      min_string += 's'
+
+    etr_string = []
+    if minutes != 0:
+      etr_string.append(min_string)
+    etr_string.append(sec_string)
+    etr_string = ', '.join(etr_string)
+
+    return etr_string
